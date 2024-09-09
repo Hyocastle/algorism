71e6b5cab7a007c12a18428c273881b7d906e60d
Hyocastle <hyoseongkim1995@gmail.com>
2024-09-09 21:32:28 +0900

    Update Main.java
    주석 추가

--- Main.java
+++ Main.java
@@ -4,6 +4,13 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main{
+
+/*
+    9개의 수를 배열에 받고
+    7개의 합이 100이 되어야하기 때문에
+    전체를 더한 뒤 2개를 빼서 100을 맞춘다.
+    이중 반복문을 사용해서 전체 sum에서 2개를 빼면서 100이 되는 값을 찾아본다.
+*/
     static int[] arr = new int[9];
     static int eight = 0, nine = 0;
     static int sum = 0;
